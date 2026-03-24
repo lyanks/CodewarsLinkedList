8509c9a58203da5555542c36cac896835ffdef99
Ulyana Kryshchuk <ulyana.kryshchuk@gmail.com>
2026-03-24 14:24:34 +0200

    fixed int values given to Nodes Parse_a_linked_list_from_a_string

--- Parse_a_linked_list_from_a_string/main.py
+++ Parse_a_linked_list_from_a_string/main.py
@@ -5,9 +5,9 @@ def linked_list_from_string(list_repr: str) -> Node | None:
     if list_repr is None or list_repr == 'None':
         return None
     values = list_repr.split(' -> ')[:-1]
-    head = Node(values[0])
+    head = Node(int(values[0]))
     cur = head
     for el in values[1:]:
-        cur.next = Node(el)
+        cur.next = Node(int(el))
         cur = cur.next
     return head
