161550c94d9b4a8239d7642716962ca6d45c636d
Ulyana Kryshchuk <ulyana.kryshchuk@gmail.com>
2026-03-24 14:02:51 +0200

    removed unnecessary element in code of convert a linked list to a string kata

--- Convert_a_linked_list_to_a_string/main.py
+++ Convert_a_linked_list_to_a_string/main.py
@@ -1,9 +1,3 @@
-class Node():
-    def __init__(self, data, next=None):
-        self.data = data
-        self.next = next
-
-
 def stringify(node):
     if node is None or node.data is None:
         return 'None'
