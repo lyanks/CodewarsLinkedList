b0655a689949de101880b5ccbc7ee10962c61143
Ulyana Kryshchuk <ulyana.kryshchuk@gmail.com>
2026-03-24 14:23:32 +0200

    testing Parse_a_linked_list_from_a_string

--- Parse_a_linked_list_from_a_string/main.py
+++ Parse_a_linked_list_from_a_string/main.py
@@ -2,4 +2,12 @@ from preloaded import Node
 
 
 def linked_list_from_string(list_repr: str) -> Node | None:
-    return None
+    if list_repr is None or list_repr == 'None':
+        return None
+    values = list_repr.split(' -> ')[:-1]
+    head = Node(values[0])
+    cur = head
+    for el in values[1:]:
+        cur.next = Node(el)
+        cur = cur.next
+    return head
