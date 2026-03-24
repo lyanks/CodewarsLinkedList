681a7169cc48045093cce1d2b5dbd351543cb825
Ulyana Kryshchuk <ulyana.kryshchuk@gmail.com>
2026-03-24 14:32:44 +0200

    Linked_Lists-Push_&_BuildOneTwoThree done and checked

---
+++ Linked_Lists-Push_&_BuildOneTwoThree/main.py
@@ -0,0 +1,14 @@
+from preloaded import Node
+
+def push(head, data):
+    node = Node(data)
+    node.next = head
+    return node
+
+
+def build_one_two_three():
+    head = None
+    head = push(head, 3)
+    head = push(head, 2)
+    head = push(head, 1)
+    return head
