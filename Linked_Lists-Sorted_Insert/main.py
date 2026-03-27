350cef238ebd06a51abaefbc44df755c16851bd3
Ulyana Kryshchuk <ulyana.kryshchuk@gmail.com>
2026-03-27 19:10:05 +0200

    Linked_Lists-Sorted_Insert added implemeentation and screenshot

---
+++ Linked_Lists-Sorted_Insert/main.py
@@ -0,0 +1,17 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+def sorted_insert(head, data):
+    new_node = Node(data)
+    if head is None or data < head.data:
+        new_node.next = head
+        return new_node
+    cur = head
+    while cur.next is not None and cur.next.data < data:
+        cur = cur.next
+    new_node.next = cur.next
+    cur.next = new_node
+    return head
